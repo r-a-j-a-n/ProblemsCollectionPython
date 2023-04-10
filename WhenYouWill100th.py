60ea15c6477fb71328a452ec7b41266433783c43
Rajan Pôudel <117752332+r-a-j-a-np-o-u-d-e-l@users.noreply.github.com>
2023-04-10 11:38:59 +0545

    #01

--- WhenYouWill100th.py
+++ WhenYouWill100th.py
@@ -1,4 +1,5 @@
 """
+
 Problem Statement:-
 Take age or year of birth as an input from the user. Store the input in one variable. Your program should detect whether the entered input is age 
 or year of birth and tell the user when they will turn 100 years old.
@@ -8,6 +9,7 @@ Here are a few instructions that you must have to follow:
          * You are not yet born
          * You seem to be the oldest person alive
          * You can also handle any other errors, if possible!
+         
          
 """
 from datetime import date
